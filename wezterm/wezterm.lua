28d3ac79d13fe3ba2ec9044bd1b02c6621bd30a1
jermaine007 <i77@idea7.top>
2023-05-16 13:45:46 +0800

    [Add] Add key mapping for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,86 @@
-local wezterm  = require 'wezterm' 
+local wezterm = require 'wezterm'
 local get_uuid = require('uuid').get_uuid
 
 local config = {
-  default_prog              = {'pwsh'},
-  color_scheme              = "Catppuccin Mocha",
-  -- enable_tab_bar            = false,
-  font_size                 = 11,
-  window_background_opacity = 0.9,
-  font                      = wezterm.font('Fira Code', { weight = 'Medium' }),
-  initial_rows              = 40,
-  initial_cols              = 120,
-  set_environment_variables = {
-      WEZTERM_SESSION = get_uuid()
-  }
+
+    default_prog              = {'pwsh'},
+    color_scheme              = "Catppuccin Mocha",
+    enable_tab_bar            = false,
+    font_size                 = 11,
+    window_background_opacity = 0.9,
+    initial_rows              = 40,
+    initial_cols              = 120,
 }
 
+config.font = wezterm.font('Cascadia Mono', { 
+    weight = 'Medium' 
+})
+
+-- Set Environment Variables
+config.set_environment_variables = {
+    WEZTERM_SESSION = get_uuid()
+}
+
+config.keys = {
+    {
+        key    = '_',
+        mods   = 'SHIFT|ALT',
+        action = wezterm.action.SplitPane {
+            direction = 'Down',
+        },
+    },
+    {
+        key    = '+',
+        mods   = 'SHIFT|ALT',
+        action = wezterm.action.SplitPane {
+            direction = 'Right',
+        },
+    },
+    {
+        key    = 'h',
+        mods   = 'SHIFT|ALT',
+        action = wezterm.action.AdjustPaneSize { 'Left', 5 }
+    },
+    {
+        key    = 'l',
+        mods   = 'SHIFT|ALT',
+        action = wezterm.action.AdjustPaneSize { 'Right', 5 }
+    },
+    {
+        key    = 'k',
+        mods   = 'SHIFT|ALT',
+        action = wezterm.action.AdjustPaneSize { 'Up', 5 }
+    },
+    {
+        key    = 'j',
+        mods   = 'SHIFT|ALT',
+        action = wezterm.action.AdjustPaneSize { 'Down', 5 }
+    },
+    {
+        key    = 'h',
+        mods   = 'CTRL|ALT',
+        action = wezterm.action.ActivatePaneDirection 'Left'
+    },
+    {
+        key    = 'l',
+        mods   = 'CTRL|ALT',
+        action = wezterm.action.ActivatePaneDirection 'Right'
+    },
+    {
+        key    = 'k',
+        mods   = 'CTRL|ALT',
+        action = wezterm.action.ActivatePaneDirection 'Up'
+    },
+    {
+        key    = 'j',
+        mods   = 'CTRL|ALT',
+        action = wezterm.action.ActivatePaneDirection 'Down'
+    },
+    {
+        key    = 'r',
+        mods   = 'ALT',
+        action = wezterm.action.ReloadConfiguration
+    },
+}
 
 return config
