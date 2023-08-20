aa0549281b7c389495c315ad6885c493e052e776
i77 <jermaine007@qq.com>
2023-08-21 07:00:45 +0800

    [ENH] Refine config lua

--- wezterm/lua/config.lua
+++ wezterm/lua/config.lua
@@ -19,19 +19,44 @@ end
 local get_uuid         = require('util').get_uuid
 local file_exists      = require('util').file_exists
 local merge            = require('util').table_merge
-local config_defaults  = {}
-local config_locals    = {}
+local user_config_defaults  = {}
+local user_config_locals    = {}
+
+--
+-- File: user_config.lua
+--
+
+-- local wezterm = require('wezterm')
+-- 
+-- return {
+--     defaults = {
+--         default_prog           = {'zsh'},
+--         status_update_interval = 1000,
+--         --font_size              = 11.0,
+--         font                   = wezterm.font_with_fallback {
+--             { family = 'JetBrains Mono', weight = 'Medium'},
+--             { family = 'Noto Mono',   weight = 'Medium' },
+--             { family = 'FiraCode NF', weight = 'Medium' },
+--             { family = 'Zhiyin',      weight = 'Medium' },
+--         },
+--         check_for_updates      = false
+--     },
+--     locals   = {
+--         use_fancy_indicator    = false,
+--         toggle_tabbar_position = false
+--     }
+-- }
 
 if file_exists(user_config_file) then
     local local_config = require('user_config')
-    config_defaults    = local_config.defaults or {}
-    config_locals      = local_config.locals or {}
+    user_config_defaults    = local_config.defaults or {}
+    user_config_locals      = local_config.locals or {}
 end
 
 local font_dirs = {wez_home_dir .. '/fonts'}
 
-local default_config = {
-    default_prog                               = {'pwsh'},
+local config_defaults = {
+    default_prog                               = is_windows and {'pwsh'} or {'zsh'},
     color_scheme                               = "Catppuccin Mocha",
     enable_tab_bar                             = true,
     font_size                                  = 10,
@@ -57,12 +82,12 @@ local default_config = {
     }
 }
 
-merge(default_config, config_defaults)
+merge(config_defaults, user_config_defaults)
 
 local setup = function(config, user_config)
 
-    merge(config,      default_config)
-    merge(user_config, config_locals)
+    merge(config,      config_defaults)
+    merge(user_config, user_config_locals)
 
 end
 
