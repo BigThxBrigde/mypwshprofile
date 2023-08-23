49897d1ebd4d5ec50ca87ddbab48608e40566bf0
jermaine007 <i77@idea7.top>
2023-08-23 09:28:23 +0800

    [ENH] Refine lsp configuration

--- simplevim/lua/vim_lsp.lua
+++ simplevim/lua/vim_lsp.lua
@@ -3,6 +3,7 @@ return {
         local cmp          = require('cmp')
         local cmp_nvim_lsp = require('cmp_nvim_lsp')
         local lspconfig    = require('lspconfig')
+        local lspkind      = require('lspkind')
         local capabilities = cmp_nvim_lsp.default_capabilities()
 
         -- languge sever to install
@@ -30,32 +31,32 @@ return {
                 end,
             },
             mapping = cmp.mapping.preset.insert(
-            {
-                ['<C-b>']     = cmp.mapping.scroll_docs(-4),
-                ['<C-f>']     = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>']     = cmp.mapping.abort(),
-                ['<CR>']      = cmp.mapping.confirm({ select = true }),
-            }),
+                {
+                    ['<C-b>']     = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>']     = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>']     = cmp.mapping.abort(),
+                    ['<CR>']      = cmp.mapping.confirm({ select = true }),
+                }),
             sources = cmp.config.sources(
-            {
-                { name = 'nvim_lsp'                 },
-                { name = 'luasnip'                  },
-                { name = 'nvim_lsp_document_symbol' }
-            },
-            {
-                { name = 'buffer' },
-            }),
+                {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    { name = 'nvim_lsp_document_symbol' }
+                },
+                {
+                    { name = 'buffer' },
+                }),
         })
 
         cmp.setup.filetype('gitcommit', {
             sources = cmp.config.sources(
-            {
-                { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-            },
-            {
-                { name = 'buffer' },
-            })
+                {
+                    { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+                },
+                {
+                    { name = 'buffer' },
+                })
         })
 
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
@@ -70,61 +71,59 @@ return {
         cmp.setup.cmdline(':', {
             mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources(
-            {
-                { name = 'path' }
-            },
-            {
-                { name = 'cmdline' }
-            })
+                {
+                    { name = 'path' }
+                },
+                {
+                    { name = 'cmdline' }
+                })
         })
 
-        local lspkind = require('lspkind')
-        cmp.setup ({
+        cmp.setup({
             formatting = {
                 format = lspkind.cmp_format({
-                mode = 'symbol_text', -- show only symbol annotations
-                maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-                ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                    mode = 'symbol_text', -- show only symbol annotations
+                    preset = 'codicons',
+                    -- maxwidth = 50,     -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                    -- ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 
-                -- The function below will be called before any actual modifications from lspkind
-                -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-                before = function (entry, vim_item)
-                    return vim_item
-                end
+                    -- -- The function below will be called before any actual modifications from lspkind
+                    -- -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                    -- before = function(entry, vim_item)
+                    --     return vim_item
+                    -- end
                 })
             }
         })
 
         for _, srv in ipairs(servers) do
+            local lsp_cfg = {
+                capabilities = capabilities,
+            }
             if srv == 'lua_ls' then
-                lspconfig[srv].setup {
-                    capabilities = capabilities,
-                    settings = {
-                        Lua = {
-                            runtime = {
-                                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                                version = 'LuaJIT'
-                            },
-                            diagnostics = {
-                                -- Get the language server to recognize the `vim` global
-                                globals = { 'vim' }
-                            },
-                            workspace = {
-                                -- Make the server aware of Neovim runtime files
-                                library = vim.api.nvim_get_runtime_file("", true)
-                            },
-                            -- Do not send telemetry data containing a randomized but unique identifier
-                            telemetry = {
-                                enable = false
-                            }
+                local settings = {
+                    Lua = {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                            version = 'LuaJIT'
+                        },
+                        diagnostics = {
+                            -- Get the language server to recognize the `vim` global
+                            globals = { 'vim' }
+                        },
+                        workspace = {
+                            -- Make the server aware of Neovim runtime files
+                            library = vim.api.nvim_get_runtime_file("", true)
+                        },
+                        -- Do not send telemetry data containing a randomized but unique identifier
+                        telemetry = {
+                            enable = false
                         }
                     }
                 }
-            else
-                lspconfig[srv].setup {
-                    capabilities = capabilities
-                }
+                rawset(lsp_cfg, 'settings', settings)
             end
+            lspconfig[srv].setup(lsp_cfg)
         end
     end
 }
