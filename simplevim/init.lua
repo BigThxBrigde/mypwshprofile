c77fbae7dc4b585ba0981984d822c957efde152b
jermaine007 <i77@idea7.top>
2023-08-25 10:55:44 +0800

    [ENH] Do not load the current configuration in vscode

--- simplevim/init.lua
+++ simplevim/init.lua
@@ -1,24 +1,3 @@
---
--- Set the lua package path
---
-
--- local pkgpath = os.getenv('SIMPLEVIM') .. '/lua/?.lua'
--- package.path  = package.path ..  pkgpath
-
--- print(package.path)
---
---
--- Install lazy.nvim if not installed
---
-require('lazynvim_setup').setup()
---
--- Load vim settings
---
-require('vim_options').setup()
---
--- Setup lazynvim
---
-local plugins      = require('plugins')
-local lazy_options = require('lazynvim_options')
-
-require('lazy').setup(plugins, lazy_options)
+if not vim.g.vscode then
+    require('simple_setup')
+end
