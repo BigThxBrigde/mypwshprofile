18a58b55381e85f42cf6eb5f8a5d994dc478883b
jermaine007 <i77@idea7.top>
2023-09-05 13:46:09 +0800

    [UPD] Use lua code to refine keymap for vscode

--- simplevim/lua/vsc/keymap.lua
+++ simplevim/lua/vsc/keymap.lua
@@ -1,18 +1,31 @@
-
+local vsc_nvim   = require('vscode-neovim')
+local vsc_call   = vsc_nvim.call
+local vsc_notify = vsc_nvim.notify
 --
 -- Execute vscode editor action
 -- vscode editor.action.action_id
 --
-local function vsc_editor_action(action_id)
-    local fmt = "<cmd>call VSCodeCall('editor.action.%s')<cr>"
-    return fmt:format(action_id)
+local function call_vsc_editor_action(action_id, ...)
+    local vararg = { ... }
+    return function()
+        local fmt = 'editor.action.%s'
+        vsc_call(fmt:format(action_id), vararg)
+    end
 end
+local function notify_vsc_editor_action(action_id, ...)
+    local vararg = { ... }
+    return function()
+        local fmt = 'editor.action.%s'
+        vsc_notify(fmt:format(action_id), vararg)
+    end
+end
+
 
 local key_defs = {
     {
         keys = {
             l = {
-                f = {vsc_editor_action('formatDocument'), 'Format Document'},
+                f = { call_vsc_editor_action('formatDocument'), 'Format Document' },
             }
         },
         opts = {
@@ -23,7 +36,7 @@ local key_defs = {
     {
         keys = {
             l = {
-                f = {vsc_editor_action('formatSelection'), 'Format Document'},
+                f = { call_vsc_editor_action('formatSelection'), 'Format Selection' },
             }
         },
         opts = {
@@ -35,18 +48,17 @@ local key_defs = {
 
         keys = {
             l = {
-                V = {vsc_editor_action('revealDefinitionAside'), 'Goto Definition Aside'},
-                d = {vsc_editor_action('revealDefinition'), 'Goto Definition'},
-                D = {vsc_editor_action('revealDeclaration'), 'Goto Declaration'},
-                v = {vsc_editor_action('showHover'), 'View Document'}
+                V = { notify_vsc_editor_action('revealDefinitionAside'), 'Goto Definition Aside' },
+                d = { notify_vsc_editor_action('revealDefinition'), 'Goto Definition' },
+                D = { notify_vsc_editor_action('revealDeclaration'), 'Goto Declaration' },
+                v = { notify_vsc_editor_action('showHover'), 'View Document' }
             }
         },
         opts = {
             prefix = '<leader>',
-            mode =  {'v', 'n'}
+            mode = { 'v', 'n' }
         }
     }
 }
+
 return key_defs
-
-
