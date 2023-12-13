1bfe025eba674ad3d5d315ed0a0debd37da881c5
i77 <i77@idea7.top>
2023-12-13 10:11:53 +0800

    [ENH] VIM: Use pwsh as default shell on windows

--- simplevim/lua/vim_options.lua
+++ simplevim/lua/vim_options.lua
@@ -40,6 +40,16 @@ function M.setup()
         ]])
     end
 
+    if  is_windows then
+        vim.cmd([[
+		let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+		let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';'
+		let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+		let &shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+		set shellquote= shellxquote=
+        ]])
+    end
+
     -- disable perl and ruby provider
     vim.cmd(
         [[
