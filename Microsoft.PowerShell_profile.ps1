18c8b8580990040fc7a1986d049cb2092f0df964
i77 <i77@idea7.top>
2024-04-17 13:14:02 +0800

    [UPD] PWSH: Change fzf color scheme in powershell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -66,13 +66,23 @@ $env:DOTNET_CLI_TELEMETRY_OPTOUT = 0
 #
 # Export fzf options
 #
+# $env:FZF_DEFAULT_OPTS = @"
+# --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
+# --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
+# --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
+# --height=70% --layout=reverse --border
+# --preview='bat -f {}'
+# --preview-window='right:60%'
+# "@
+#
 $env:FZF_DEFAULT_OPTS = @"
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
---color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
---height=70% --layout=reverse --border
---preview='bat -f {}'
---preview-window='right:60%'
+  --color=spinner:#f4dbd6,hl:#ed8796
+  --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6
+  --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796
+  --height=60%
+  --layout=reverse
+  --preview='bat -f {}'
+  --preview-window='right:60%'
 "@
 
 $env:EDITOR = 'nvim'
