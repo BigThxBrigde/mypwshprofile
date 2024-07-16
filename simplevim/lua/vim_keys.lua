f3e64d956efafc7f324422ab1145df797c840dd1
i77 <i77@idea7.top>
2024-07-16 10:57:28 +0800

    [ADD] VIM: Add some icons and change which-key style

--- simplevim/lua/vim_keys.lua
+++ simplevim/lua/vim_keys.lua
@@ -3,6 +3,17 @@ local key_defs = pload('keymap')
 
 return {
     setup = function()
+        wk.setup({
+            preset = 'helix',
+            icons  = {
+                rules = {
+                    { pattern = "language", icon = "󰃤 ", color = "red" },
+                    { pattern = "comments", icon = " ", color = "orange" },
+                    { pattern = "extensions", icon = "󰅇", color = "green" },
+                    { pattern = "motions", icon = " ", color = "red" },
+                }
+            }
+        })
         for _, mappings in ipairs(key_defs) do
             wk.add(mappings)
         end
